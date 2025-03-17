fe3755eb9f9472e495a8ce04ddf8226d6aea6ca6
Filipe062 <luisfhylipe@gmail.com>
2025-03-17 16:21:07 -0300

    quinto

--- gitfind/src/pages/home/index.js
+++ gitfind/src/pages/home/index.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import { Header } from "../../components/Header";
 import background from "../../assets/background.png";
 import ItemList from "../../components/ItemList";
 import "./styles.css";
 
 function App() {
+  const [user, setUser] = useState("");
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+
+    if (newUser.name) {
+      const { avatar_url, name, bio } = newUser;
+      setCurrentUser({ avatar_url, name, bio });
+
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+      const newRepos = await reposData.json();
+
+      if (newRepos.length) {
+        setRepos(newRepos);
+      }
+    }
+  };
+
   return (
     <div className="App">
       <Header />
@@ -12,28 +34,39 @@ function App() {
 
         <div className="info">
           <div>
-            <input name="usuario" placeholder="@usuario" />
-            <button>Buscar</button>
+            <input
+              name="usuario"
+              value={user}
+              onChange={(event) => setUser(event.target.value)}
+              placeholder="@usuario"
+            />
+            <button onClick={handleGetData}>Buscar</button>
           </div>
 
-          <div className="perfil">
-            <img
-              src="https://avatars.githubusercontent.com/u/186089519?s=400&u=3086cb09f80216cefdc3866f127cf29b9f73b07c&v=4"
-              className="profile"
-              alt="Foto de perfil do usuário"
-            />
-            <div>
-              <h3>Luiz Filipe</h3>
-              <span>@filipe062</span>
-              <p>Descrição</p>
-            </div>
-          </div>
-          <hr />
+          {currentUser && (
+            <>
+              <div className="perfil">
+                <img
+                  src={currentUser.avatar_url}
+                  className="profile"
+                  alt="Foto de perfil do usuário"
+                />
+                <div>
+                  <h3>{currentUser.name}</h3>
+                  <span>@{currentUser.login}</span>
+                  <p>{currentUser.bio}</p>
+                </div>
+              </div>
+              <hr />
+            </>
+          )}
+
           <div>
             <h4 className="repositorio">Repositórios</h4>
-            <ItemList title="teste" description="teste de descriçao" />
-            <ItemList title="teste" description="teste de descriçao" />
-            <ItemList title="teste" description="teste de descriçao" />
+            {repos &&
+              repos.map((repo) => (
+                <ItemList key={repo.id} title={repo.name} description={repo.description} />
+              ))}
           </div>
         </div>
       </div>
