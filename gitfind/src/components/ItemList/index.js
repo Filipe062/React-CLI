1cf75174f5bf3eefc4e7bb8a8e9b7c33b18748e8
Filipe062 <luisfhylipe@gmail.com>
2025-03-17 14:52:05 -0300

    quarto

---
+++ gitfind/src/components/ItemList/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import React from "react"
+import "./styles.css"
+
+function ItemList({title, description}) {
+  return (
+        <div className="item-list">
+            <strong>{title}</strong>
+            <p>{description}</p>
+            <hr />
+         </div>
+  );
+}
+
+export default ItemList;
--- gitfind/src/pages/home/index.js
+++ gitfind/src/pages/home/index.js
@@ -1,24 +1,41 @@
 import { Header } from "../../components/Header";
 import background from "../../assets/background.png";
-
-import "./styles.css"
+import ItemList from "../../components/ItemList";
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="conteudo">
-        <img 
-          src={background} 
-          className="background"
-          alt="Imagem de fundo"  />
-          <div className="info">
-             <div>
-               <input name="usuaro" placeholder="@usuario" />
-               <button>Buscar</button>
-             </div>
+        <img src={background} className="background" alt="Imagem de fundo" />
+
+        <div className="info">
+          <div>
+            <input name="usuario" placeholder="@usuario" />
+            <button>Buscar</button>
           </div>
 
+          <div className="perfil">
+            <img
+              src="https://avatars.githubusercontent.com/u/186089519?s=400&u=3086cb09f80216cefdc3866f127cf29b9f73b07c&v=4"
+              className="profile"
+              alt="Foto de perfil do usuário"
+            />
+            <div>
+              <h3>Luiz Filipe</h3>
+              <span>@filipe062</span>
+              <p>Descrição</p>
+            </div>
+          </div>
+          <hr />
+          <div>
+            <h4 className="repositorio">Repositórios</h4>
+            <ItemList title="teste" description="teste de descriçao" />
+            <ItemList title="teste" description="teste de descriçao" />
+            <ItemList title="teste" description="teste de descriçao" />
+          </div>
+        </div>
       </div>
     </div>
   );
