96129f340044e47abc2c653089e4343e8d46c4e5
Filipe062 <luisfhylipe@gmail.com>
2025-03-17 10:27:36 -0300

    terceiro

--- gitfind/src/index.js
+++ gitfind/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import App from "./src/home";
+import './style.css';
+import App from "./pages/home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- gitfind/src/pages/home/index.js
+++ gitfind/src/pages/home/index.js
@@ -1,7 +1,27 @@
-import React from "react";
+import { Header } from "../../components/Header";
+import background from "../../assets/background.png";
+
+import "./styles.css"
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="conteudo">
+        <img 
+          src={background} 
+          className="background"
+          alt="Imagem de fundo"  />
+          <div className="info">
+             <div>
+               <input name="usuaro" placeholder="@usuario" />
+               <button>Buscar</button>
+             </div>
+          </div>
+
+      </div>
+    </div>
+  );
 }
 
 export default App;
